ce28e34de43f2ea580116508207bb6807c439bea
DavideBlade <davideblade99@gmail.com>
2022-04-24 19:49:38 +0200

    Version 1.18.2 added

---
+++ v1_18_R2/src/main/java/com/gmail/davideblade99/fullcloak/nms/Title_v1_18_R2.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) DavideBlade.
+ *
+ * All Rights Reserved.
+ */
+
+package com.gmail.davideblade99.fullcloak.nms;
+
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.chat.IChatBaseComponent.ChatSerializer;
+import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSetTitlesAnimationPacket;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public final class Title_v1_18_R2 implements Title {
+
+    @Override
+    public void sendTitle(final Player player, final String msgTitle, final String msgSubTitle, final int ticks) {
+        // When tested, Player#sendTitle() did not work properly
+
+        final ClientboundSetTitlesAnimationPacket times = new ClientboundSetTitlesAnimationPacket(20, ticks, 20);
+        ((CraftPlayer) player).getHandle().b.a(times);
+
+        final IChatBaseComponent chatTitle = ChatSerializer.a("{\"text\": \"" + msgTitle + "\"}");
+        final ClientboundSetTitleTextPacket title = new ClientboundSetTitleTextPacket(chatTitle);
+        ((CraftPlayer) player).getHandle().b.a(title);
+
+        final IChatBaseComponent chatSubTitle = ChatSerializer.a("{\"text\": \"" + msgSubTitle + "\"}");
+        final ClientboundSetSubtitleTextPacket subtitle = new ClientboundSetSubtitleTextPacket(chatSubTitle);
+        ((CraftPlayer) player).getHandle().b.a(subtitle);
+    }
+}
